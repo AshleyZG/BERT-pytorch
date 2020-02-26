46ab2ca2e75639530cddfdfaab251d12f3524662
AshleyZG <gsarazh@gmail.com>
2020-02-26 14:48:49 +0800

    main script

--- bert_pytorch/__main__.py
+++ bert_pytorch/__main__.py
@@ -4,12 +4,12 @@ from torch.utils.data import DataLoader
 
 from model import BERT
 from trainer import BERTTrainer, ReconstructionBERTTrainer
-from dataset import BERTDataset, WordVocab, DataReader, Vocab, my_collate, CustomBERTDataset
+from dataset import BERTDataset, WordVocab, DataReader, Vocab, MarkdownVocab, my_collate, CustomBERTDataset
 import pdb
 import os
+import json
 
 import torch
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def train():
@@ -17,16 +17,15 @@ def train():
 
     parser.add_argument("-c", "--dataset", required=True,
                         type=str, help="dataset")
-    # parser.add_argument("-c", "--train_dataset", required=True,
-    #                     type=str, help="train dataset for train bert")
-    # parser.add_argument("-t", "--test_dataset", type=str,
-    #                     default=None, help="test set for evaluate train set")
-    # parser.add_argument("-v", "--vocab_path", required=True,
-    #                     type=str, help="built vocab model path with bert-vocab")
+
     parser.add_argument("-o", "--output_path", required=True,
+                        type=str, help="ex)output/bert.model")
+    parser.add_argument("-t", "--test_path", required=True,
                         type=str, help="ex)output/bert.model")
 
     parser.add_argument("-hs", "--hidden", type=int,
+                        default=256, help="hidden size of transformer model")
+    parser.add_argument("-me", "--markdown_emb_size", type=int,
                         default=256, help="hidden size of transformer model")
     parser.add_argument("-l", "--layers", type=int,
                         default=8, help="number of layers")
@@ -34,6 +33,9 @@ def train():
                         default=8, help="number of attention heads")
     parser.add_argument("-s", "--seq_len", type=int,
                         default=64, help="maximum sequence len")
+
+    parser.add_argument("--hinge_loss_start_point", type=int, default=20)
+    parser.add_argument("--entropy_start_point", type=int, default=30)
 
     parser.add_argument("-b", "--batch_size", type=int,
                         default=64, help="number of batch_size")
@@ -43,18 +45,22 @@ def train():
                         default=5, help="dataloader worker size")
     parser.add_argument("--duplicate", type=int,
                         default=5, help="dataloader worker size")
+    parser.add_argument("--model_path",
+                        type=str, help="ex)output/bert.model")
 
     parser.add_argument("--with_cuda", type=bool, default=True,
                         help="training with CUDA: true, or false")
     parser.add_argument("--log_freq", type=int, default=10,
                         help="printing loss every n iter: setting n")
+    parser.add_argument("--max_graph_num", type=int, default=3000000,
+                        help="printing loss every n iter: setting n")
     parser.add_argument("--corpus_lines", type=int,
                         default=None, help="total number of lines in corpus")
-    parser.add_argument("--cuda_devices", type=int, nargs='+',
-                        default=None, help="CUDA device ids")
+    parser.add_argument("--cuda_devices", type=str,
+                        default='0', help="CUDA device ids")
     parser.add_argument("--on_memory", type=bool, default=True,
                         help="Loading on memory: true or false")
-    parser.add_argument("--n_topics", type=int, default=10)
+    parser.add_argument("--n_topics", type=int, default=50)
     parser.add_argument("--lr", type=float, default=1e-3,
                         help="learning rate of adam")
     parser.add_argument("--adam_weight_decay", type=float,
@@ -70,56 +76,61 @@ def train():
                         help="force topic distribution over neighbor nodes to be close")
     parser.add_argument("--min_occur", type=int,
                         default=3, help="minimum of occurrence")
-    # parser.add_argument("--tolerance", type=int,
-    #                     default=2, help="minimum of occurrence")
+
     parser.add_argument("--use_sub_token", action="store_true")
+    parser.add_argument("--context", action="store_true",
+                        help="use information from neighbor cells")
+    parser.add_argument("--markdown", action="store_true", help="use markdown")
     args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda_devices
 
     print("Load Data", args.dataset)
     data_reader = DataReader(
-        args.dataset, seq_len=args.seq_len, use_sub_token=args.use_sub_token)
-    neg_data_reader = DataReader(
-        args.dataset, graphs=data_reader.graphs, shuffle=True, duplicate=args.duplicate, seq_len=args.seq_len)
+        args.dataset, seq_len=args.seq_len, use_sub_token=args.use_sub_token, max_graph_num=args.max_graph_num)
+
+    labeled_data_reader = DataReader(
+        args.test_path, use_sub_token=args.use_sub_token)
+
     # print("Loading Vocab", args.vocab_path)
     print("Loading Vocab")
     vocab = Vocab(data_reader.graphs, min_occur=args.min_occur,
                   use_sub_token=args.use_sub_token)
-    # vocab = WordVocab.load_vocab(args.vocab_path)
+    markdown_vocab = MarkdownVocab(
+        data_reader.graphs, min_occur=args.min_occur)
     print("Vocab Size: ", len(vocab))
+    print("MarkdownVocab Size: ", len(markdown_vocab))
 
     print("Shuffle Data")
     'TODO'
 
     print("Loading Train Dataset", args.dataset)
-    train_dataset = CustomBERTDataset(data_reader.graphs[:int(len(data_reader) * 0.8)], vocab, seq_len=args.seq_len,
-                                      on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token)
-    # pdb.set_trace()
+    train_dataset = CustomBERTDataset(data_reader.graphs[:int(len(data_reader) * 0.8)], vocab, markdown_vocab, seq_len=args.seq_len,
+                                      on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token, n_topics=args.n_topics)
+
     print(len(train_dataset))
-    neg_train_dataset = CustomBERTDataset(neg_data_reader.graphs[:args.duplicate * len(train_dataset)], vocab, seq_len=args.seq_len,
-                                          on_memory=args.on_memory, n_neg=args.duplicate)
-    # pdb.set_trace()
-    assert len(neg_train_dataset) == args.duplicate * len(train_dataset)
+
     # print("Loading Test Dataset", args.test_dataset)
     print("Loading Dev Dataset", args.dataset)
-    test_dataset = CustomBERTDataset(data_reader.graphs[int(len(data_reader) * 0.8):], vocab, seq_len=args.seq_len,
-                                     on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token)  # \
+    test_dataset = CustomBERTDataset(data_reader.graphs[int(len(data_reader) * 0.8):], vocab, markdown_vocab, seq_len=args.seq_len,
+                                     on_memory=args.on_memory, n_neg=args.duplicate, use_sub_token=args.use_sub_token, n_topics=args.n_topics)  # \
     print(len(test_dataset))
 
-    neg_test_dataset = CustomBERTDataset(neg_data_reader.graphs[-args.duplicate * len(test_dataset):], vocab, seq_len=args.seq_len,
-                                         on_memory=args.on_memory, n_neg=args.duplicate)  # \
-    assert len(neg_test_dataset) == args.duplicate * len(test_dataset)
+    labeled_dataset = CustomBERTDataset(labeled_data_reader.graphs, vocab, markdown_vocab, seq_len=args.seq_len,
+                                        on_memory=args.on_memory, use_sub_token=args.use_sub_token)
+
     # if args.test_dataset is not None else None
     # pdb.set_trace()
     print("Creating Dataloader")
     train_data_loader = DataLoader(
         train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)
-    neg_train_data_loader = DataLoader(
-        neg_train_dataset, batch_size=args.batch_size * args.duplicate, num_workers=args.num_workers, collate_fn=my_collate)
 
     test_data_loader = DataLoader(
         test_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)  # \
-    neg_test_data_loader = DataLoader(
-        neg_test_dataset, batch_size=args.batch_size * args.duplicate, num_workers=args.num_workers, collate_fn=my_collate)  # \
+
+    labeled_data_loader = DataLoader(
+        labeled_dataset, batch_size=args.batch_size, num_workers=args.num_workers, collate_fn=my_collate)
+
     # if test_dataset is not None else None
     # assert False
     print("Building BERT model")
@@ -131,25 +142,62 @@ def train():
     #                       lr=args.lr, betas=(
     #                           args.adam_beta1, args.adam_beta2), weight_decay=args.adam_weight_decay,
     #                       with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, pad_index=vocab.pad_index)
-    trainer = ReconstructionBERTTrainer(bert, len(vocab), train_dataloader=train_data_loader, test_dataloader=test_data_loader, neg_train_dataloader=neg_train_data_loader, neg_test_dataloader=neg_test_data_loader,
+    trainer = ReconstructionBERTTrainer(bert, len(vocab), len(markdown_vocab), args.markdown_emb_size, train_dataloader=train_data_loader, test_dataloader=test_data_loader,
                                         lr=args.lr, betas=(
         args.adam_beta1, args.adam_beta2), weight_decay=args.adam_weight_decay,
-        with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, pad_index=vocab.pad_index, weak_supervise=args.weak_supervise)
+        with_cuda=args.with_cuda, cuda_devices=args.cuda_devices, log_freq=args.log_freq, pad_index=vocab.pad_index, model_path=args.model_path, weak_supervise=args.weak_supervise, context=args.context, markdown=args.markdown, hinge_loss_start_point=args.hinge_loss_start_point, entropy_start_point=args.entropy_start_point)
     # raise NotImplementedError
     print("Training Start")
+    output_folder = args.output_path.split('.')[0]
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    with open(os.path.join(output_folder, './setting.json'), 'w') as fout:
+        json.dump(args.__dict__, fout, ensure_ascii=False, indent=2)
+    # pdb.set_trace()
     best_loss = None
+
     for epoch in range(args.epochs):
+        if trainer.last_epoch and epoch <= trainer.last_epoch:
+            continue
         train_loss = trainer.train(epoch)
 
         # if test_data_loader is not None:
         test_loss = trainer.test(epoch)
-        if epoch == 2 or epoch == 3:
-            best_loss = None
+        # if epoch == 2 or epoch == 3:
+        #     best_loss = None
+        # if epoch <= 5:
+        #     best_loss = None
         # if epoch == 20:
         #     best_loss = None
-        if best_loss is None or test_loss < best_loss:
-            best_loss = test_loss
-            trainer.save(epoch, args.output_path)
+        trainer.save(epoch, os.path.join(output_folder, args.output_path))
+        # if best_loss is None or test_loss < best_loss:
+        #     best_loss = test_loss
+        ids, stages, stage_vecs = trainer.api(labeled_data_loader)
+
+        correct = 0
+        zero_cells = 0
+        for g in labeled_dataset.graphs:
+            if int(g["stage"]) == 0:
+                zero_cells += 1
+        for i, g in enumerate(labeled_dataset.graphs):
+            if stages[i] == int(g["stage"]) and int(g["stage"]) != 0:
+                correct += 1
+            else:
+                pass
+        accuracy = correct / (len(stages) - zero_cells)
+
+        with open(os.path.join(output_folder, './results.txt'), 'a') as fout:
+            json.dump({"epoch": epoch,
+                       "accuracy": accuracy,
+                       "loss": test_loss}, fout)
+            fout.write('\n')
+
+        with open(os.path.join(output_folder, 'graphs_{}.txt'.format(epoch)), 'w') as fout:
+            for i, g in enumerate(labeled_dataset.graphs):
+                g["pred"] = stages[i]
+                g["stage_vec"] = stage_vecs[i]
+                fout.write(json.dumps(g))
+                fout.write('\n')
 
         torch.cuda.empty_cache()
 
